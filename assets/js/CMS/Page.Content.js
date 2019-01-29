da43aa2574340aac6af452d6fb20f60c3a22dad6
NEAL-DESKTOP\Neal-Geilen <nealgeilen@gmail.com>
2019-01-29 21:47:13 +0100

    Handles added //TODO diffrend  fields setup

--- assets/js/CMS/Page.Content.js
+++ assets/js/CMS/Page.Content.js
@@ -13,7 +13,7 @@ var Builder = {
             $(sSelectors.Content_Type_Selector).draggable({
                 helper: function () {
                     oTemplate = $($('.template-page-block').html());
-                    Builder.Block.resizable(oTemplate);
+                    Builder.Block.Resizable.resizable(oTemplate);
                     return oTemplate;
                 },
                 connectToSortable: sSelectors.Group_Content,
@@ -42,17 +42,16 @@ var Builder = {
       }
     },
     Group: {
-        build : function(){
+        Build : function(){
             oTemplate = $($('.template-page-group').html());
             this.sortable(oTemplate.find(sSelectors.Group_Content));
             return oTemplate;
         },
-        add: function () {
-            // iPostion = $(sSelectors.Editor + ' > ' + sSelectors.Group).length;
-            $(sSelectors.Editor).append(this.build());
+        Add: function () {
+            $(sSelectors.Editor).append(this.Build());
         },
-        delete : function () {
-
+        Delete : function (oElement) {
+            oElement.remove();
         },
         sortable: function (oElement = null) {
             Selector = (oElement === null) ? $(sSelectors.Group_Content) : oElement;
@@ -63,42 +62,53 @@ var Builder = {
         }
     },
     Block: {
-        resizable: function (oElement = null) {
-            Selector = (oElement === null) ? $(sSelectors.Block_Item) : oElement;
-            B = this;
-            Selector.resizable({
-                handle: {e : '.Block-Item-Width-Control'},
-                start: function (event, ui) {
-                    B.ResizePermission(ui);
-                },
-                resize: function (event,ui) {
-                    B.ResizePermission(ui);
-                    B.OnResize(ui);
-                },
-                stop: function (event,ui) {
-                    B.ResizePermission(ui);
+        Resizable: {
+            resizable: function (oElement = null) {
+                Selector = (oElement === null) ? $(sSelectors.Block_Item) : oElement;
+                B = this;
+                Selector.resizable({
+                    handles: {
+                        e : '.Block-Item-Width-Control'
+                    },
+                    start: function (event, ui) {
+                        B.ResizePermission(ui);
+                    },
+                    resize: function (event,ui) {
+                        B.ResizePermission(ui);
+                        B.OnResize(ui);
+                    },
+                    stop: function (event,ui) {
+                        B.ResizePermission(ui);
+                    }
+                });
+            },
+            OnResize: function (ui) {
+                Element = $(ui.helper);
+                iWidth = parseInt(Element.attr('block-width'));
+                Element.removeClass('col-' + iWidth);
+                i = parseInt((Element.width() / ($(sSelectors.Group_Content).innerWidth() / 100 * (100/12))));
+                Element.addClass('col-' + i).css('width', (100/12*i).toFixed() + '%').attr('block-width', i);
+            },
+            ResizePermission(ui){
+                Element = $(ui.item);
+                if (parseInt(Element.attr('block-width')) > 12) {
+                    Element.resizable("option", "maxWidth", Element.width());
+                } else {
+                    Element.resizable("option", "maxWidth", null);
                 }
-            });
+            },
         },
-        OnResize: function (ui) {
-            Element = $(ui.helper);
-            iWidth = parseInt(Element.attr('block-width'));
-            Element.removeClass('col-' + iWidth);
-            i = parseInt((Element.width() / ($(sSelectors.Group_Content).innerWidth() / 100 * (100/12))));
-            Element.addClass('col-' + i).css('width', (100/12*i).toFixed() + '%').attr('block-width', i);
+        Delete: function(oElement){
+            oElement.remove();
         },
-        ResizePermission(ui){
-            Element = $(ui.item);
-            if (parseInt(Element.attr('block-width')) > 12) {
-                Element.resizable("option", "maxWidth", Element.width());
-            } else {
-                Element.resizable("option", "maxWidth", null);
+        Active: {
+            Set: function(iContentBlockId){
+                
+            },
+            Remove: function () {
+
             }
-        }
-
-    },
-    Content:{
-
+        },
     },
     Item:{
 
