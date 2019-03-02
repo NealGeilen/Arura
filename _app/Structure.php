e72d36405841d7393b35537b2f14f8b67d183ed8
NEAL-DESKTOP\Neal-Geilen <nealgeilen@gmail.com>
2019-03-02 12:30:32 +0100

    Structure file change

---
+++ _app/Structure.php
@@ -0,0 +1,9 @@
+<?php
+define('__ROOT__',       $_SERVER['DOCUMENT_ROOT']);
+define('__APP__',        __ROOT__ . '/_app/');
+//define('__INCLUDE__',    __ROOT__ . '/_include/');
+define('__ADDONS__',   __ROOT__ . '/_Addons/');
+define('__VENDOR__',     __ROOT__ . '/vendor/');
+define('__RESOURCES__',  __APP__  . 'Resources/');
+define('__APP_ROOT__',   realpath(__ROOT__ . '/../../'));
+define('__SETTINGS__',   __APP_ROOT__ . '/settings/');
--- _app/autoload.php
+++ _app/autoload.php
@@ -1,3 +1,4 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once "Rights.php";
+require_once "Structure.php";
