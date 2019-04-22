c0321731f9e01c5ec2eafbdcafaabaf3c3b6b450
NEAL-DESKTOP\Neal-Geilen <nealgeilen@gmail.com>
2019-04-22 22:17:41 +0200

    sql dump

--- webserver.sql
+++ webserver.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 12 mrt 2019 om 10:30
+-- Gegenereerd op: 22 apr 2019 om 20:16
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `webserver`
 --
+CREATE DATABASE IF NOT EXISTS `webserver` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `webserver`;
 
 -- --------------------------------------------------------
 
@@ -89,29 +91,32 @@ CREATE TABLE IF NOT EXISTS `tblcmscontentblocks` (
   `Content_Position` int(11) DEFAULT NULL,
   `Content_Size` int(11) DEFAULT NULL,
   `Content_Raster` int(11) DEFAULT '2',
-  `Content_Css_Background_Color` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `Content_Css_Background_Color` varchar(11) COLLATE utf8_bin DEFAULT NULL,
   `Content_Css_Background_Img` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`Content_Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=93 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tblcmscontentblocks`
 --
 
 INSERT INTO `tblcmscontentblocks` (`Content_Id`, `Content_Group_Id`, `Content_Addon_Id`, `Content_Type`, `Content_Value`, `Content_Position`, `Content_Size`, `Content_Raster`, `Content_Css_Background_Color`, `Content_Css_Background_Img`) VALUES
-(23, 37, 0, 'TextArea', '<h2 style=\"text-align: center;\">Over Ons</h2>\n<p style=\"text-align: center;\">Dakwerken John Geilen &ndash; derde generatie dakdekkers inmiddels &ndash; een bedrijf dat o.a. gespecialiseerd is in platte en gebitumineerde daken. Het aanleggen van lood &ndash; en zinkwerken is bij ons bedrijf in uitstekende handen. Tevens verzorgen wij reparaties aan pannendaken en voeren schoorsteenrenovaties uit.</p>', 0, 12, 2, '', ''),
-(16, 31, 5, 'widget', '[{\"img\":\"\\/assets\\/img\\/home-head-top-kwaliteit-1.jpg\",\"text\":\"<h4 class=\\\"text-primary\\\" style=\\\"text-align: center;\\\">TOP KWALITEIT<\\/h4>\\n<p style=\\\"text-align: center;\\\">Wij werken alleen met de allerbeste materialen die voorzien zijn van uitgebreide garanties. Dat verhoogt aanzienlijk de levensduur van uw dak. Bij ons kunt u terecht voor het onderhoud van daken en goten. Net als een tuin, een huis of een auto heeft uw dak of dakgoot een jaarlijkse onderhoudsbeurt nodig. Onderhoud verlengt de levensduur van daken en goten. U voorkomt er erg grote en dure reparaties mee en investeert in de kwaliteit van uw huis of bedrijfspand.<\\/p>\"},{\"img\":\"\\/assets\\/img\\/home-head-zink-lood-werk.jpg\",\"text\":\"<h4 class=\\\"text-primary\\\" style=\\\"text-align: center;\\\">LOOD- EN ZINKWERK<\\/h4>\\n<p style=\\\"text-align: center;\\\">Goten en hemelwaterafvoeren van zink en koper zijn reeds jaren algemeen bekend. Maar ook complete daken en gevels worden uitgevoerd met lood, zink of koper. De bewezen levensduur van deze materialen in combinatie met een goede montage is indrukwekkend. Er zijn bijvoorbeeld dak- of gevelbekledingen die ouder zijn dan 500 jaar. Dit is een reden te meer om eens serieus over dergelijk systeem te gaan nadenken. Wij denken graag met U mee.<\\/p>\"},{\"img\":\"\\/assets\\/img\\/home-head-bitumineuze-dakbedekking.jpg\",\"text\":\"<h4 class=\\\"text-primary\\\" style=\\\"text-align: center;\\\">BITUMINEUZE DAKBEDEKKING<\\/h4>\\n<p style=\\\"text-align: center;\\\">Wij werken alleen met IKO producten, (voorheen Nebiprofa) dat sinds 1942 toonaangevend fabrikant van duurzame dakconcepten is. Dankzij onze jarenlange ervaring en brede assortiment dakbedekkingsmaterialen (IKO), isolatie en dakaccessoires bieden wij totaaloplossingen voor elk dak. Hierbij hebben wij oog voor duurzaamheid, gebruiksvriendelijkheid en kwaliteit van onze producten in het eindgebruik van onze dakconcepten.<\\/p>\"}]', 0, 12, 4, '', ''),
-(22, 36, 0, 'TextArea', '<h1 style=\"text-align: center;\">Dakwerken John Geilen</h1>', 0, 12, 4, '', ''),
-(24, 37, 0, 'Picture', '/assets/img/john_geiler.jpg', 2, 4, 2, '', ''),
-(26, 37, 0, 'Picture', '/assets/img/john_geilen_history.jpg', 1, 4, 2, '', ''),
-(27, 37, 0, 'Picture', '/assets/img/john_geilen_history_logo.jpg', 3, 4, 2, '', ''),
-(28, 38, 0, 'TextArea', '<h1 style=\"text-align: center;\">Onze partners</h1>', 0, 12, 2, '', ''),
-(29, 38, 0, 'Picture', '/assets/img/vmzinc-logo.png', 3, 4, 2, '', ''),
-(34, 38, 0, 'Picture', '/assets/img/Iko-logo.jpg', 4, 4, 2, '', ''),
-(35, 38, 0, 'Picture', '/assets/img/Sievert-logo.jpg', 5, 4, 2, '', ''),
-(37, 38, 0, 'Picture', '/assets/img/Velux-logo.jpg', 1, 4, 2, '', ''),
-(38, 38, 0, 'Picture', '/assets/img/Hilti-logo.jpg', 2, 4, 2, '', ''),
-(39, 38, 0, 'Picture', '/assets/img/Rheinzink-logo.jpg', 6, 4, 2, '', '');
+(76, 47, 0, 'Picture', 'vmzinc.png', 2, 4, 2, '', ''),
+(77, 47, 0, 'Picture', 'velux.png', 3, 4, 2, '', ''),
+(78, 47, 0, 'TextArea', '<h1 class=\"text-center text-primary\" style=\"margin-top: 0px; margin-bottom: 0.5rem; font-family: Montserrat; font-weight: bold; line-height: 1.2; font-size: 2.5rem; letter-spacing: normal; color: navy !important;\">Onze partners</h1>', 0, 12, 2, '', ''),
+(73, 47, 0, 'Picture', 'sievert.png', 4, 4, 2, '', ''),
+(74, 47, 0, 'Picture', 'reinzink.png', 5, 4, 2, '', ''),
+(71, 47, 0, 'Picture', 'iko.png', 6, 4, 2, '', ''),
+(72, 47, 0, 'Picture', 'Hilti.png', 1, 4, 2, '', ''),
+(79, 48, 5, 'widget', '[{\"img\":\"home-head-top-kwaliteit-1.jpg\",\"text\":\"<h4 class=\\\"text-primary\\\" style=\\\"margin-top: 0px; margin-bottom: 0.5rem; font-family: Montserrat; font-weight: bold; line-height: 1.2; font-size: 1.5rem; text-align: center; color: navy !important;\\\">TOP KWALITEIT<\\/h4>\\n<p style=\\\"margin-bottom: 1rem; color: #000000; font-family: Lato; font-size: 16px; text-align: center;\\\">Wij werken alleen met de allerbeste materialen die voorzien zijn van uitgebreide garanties. Dat verhoogt aanzienlijk de levensduur van uw dak. Bij ons kunt u terecht voor het onderhoud van daken en goten. Net als een tuin, een huis of een auto heeft uw dak of dakgoot een jaarlijkse onderhoudsbeurt nodig. Onderhoud verlengt de levensduur van daken en goten. U voorkomt er erg grote en dure reparaties mee en investeert in de kwaliteit van uw huis of bedrijfspand.<\\/p>\"},{\"img\":\"home-head-zink-lood-werk.jpg\",\"text\":\"<h4 class=\\\"text-primary\\\" style=\\\"margin-top: 0px; margin-bottom: 0.5rem; font-family: Montserrat; font-weight: bold; line-height: 1.2; font-size: 1.5rem; text-align: center; color: navy !important;\\\">LOOD- EN ZINKWERK<\\/h4>\\n<p style=\\\"margin-bottom: 1rem; color: #000000; font-family: Lato; font-size: 16px; text-align: center;\\\">Goten en hemelwaterafvoeren van zink en koper zijn reeds jaren algemeen bekend. Maar ook complete daken en gevels worden uitgevoerd met lood, zink of koper. De bewezen levensduur van deze materialen in combinatie met een goede montage is indrukwekkend. Er zijn bijvoorbeeld dak- of gevelbekledingen die ouder zijn dan 500 jaar. Dit is een reden te meer om eens serieus over dergelijk systeem te gaan nadenken. Wij denken graag met U mee.<\\/p>\"},{\"img\":\"home-head-bitumineuze-dakbedekking.jpg\",\"text\":\"<h4 class=\\\"text-primary\\\" style=\\\"margin-top: 0px; margin-bottom: 0.5rem; font-family: Montserrat; font-weight: bold; line-height: 1.2; font-size: 1.5rem; text-align: center; color: navy !important;\\\">BITUMINEUZE DAKBEDEKKING<\\/h4>\\n<p style=\\\"margin-bottom: 1rem; color: #000000; font-family: Lato; font-size: 16px; text-align: center;\\\">Wij werken alleen met IKO producten, (voorheen Nebiprofa) dat sinds 1942 toonaangevend fabrikant van duurzame dakconcepten is. Dankzij onze jarenlange ervaring en brede assortiment dakbedekkingsmaterialen (IKO), isolatie en dakaccessoires bieden wij totaaloplossingen voor elk dak. Hierbij hebben wij oog voor duurzaamheid, gebruiksvriendelijkheid en kwaliteit van onze producten in het eindgebruik van onze dakconcepten.<\\/p>\"}]', 0, 12, 4, '', ''),
+(80, 49, 0, 'TextArea', '<h1 style=\"text-align: center;\">Over ons</h1>\n<p style=\"text-align: center;\">Dakwerken John Geilen &ndash; derde generatie dakdekkers inmiddels &ndash; een bedrijf dat o.a. gespecialiseerd is in platte en gebitumineerde daken. Het aanleggen van lood &ndash; en zinkwerken is bij ons bedrijf in uitstekende handen. Tevens verzorgen wij reparaties aan pannendaken en voeren schoorsteenrenovaties uit.</p>\n<p style=\"text-align: center;\">&nbsp;</p>\n<p style=\"text-align: center;\">&nbsp;</p>', 0, 12, 2, '', ''),
+(83, 49, 0, 'Picture', 'John-Geilen-Oud.png', 1, 4, 2, '', ''),
+(92, 50, 0, 'TextArea', '<h1 style=\"text-align: center;\">Dakwerken John Geilen</h1>', 0, 12, 2, NULL, NULL),
+(85, 49, 0, 'Picture', 'John-Geilen.png', 2, 4, 2, '', ''),
+(86, 49, 0, 'Picture', 'logo-oud.png', 3, 4, 2, '', ''),
+(89, 51, 0, 'TextArea', '<h3 style=\"text-align: center;\">Ons bedrijf</h3>\n<h1 style=\"text-align: center;\">In cijfers</h1>', 0, 12, 2, '', ''),
+(90, 52, 0, 'TextArea', '<h1 style=\"text-align: center;\">Projecten</h1>', 0, 12, 2, '', ''),
+(91, 53, 0, 'TextArea', '<h1 style=\"text-align: center;\">Contact</h1>\n<p>Wij zijn telefonisch bereikbaar van maandag t/m zaterdag van 8.00 tot 18.00 uur. Wij werken voor particulieren en voor bedrijven. Offertes worden kosteloos gemaakt en we berekenen geen voorrijkosten.</p>\n<p>&nbsp;</p>\n<p>06-53667219<br />046-737008<br />info@dwjg.nl<br />Verlengde Heinseweg 5<br />6136 AP Sittard</p>', 0, 12, 2, '', '');
 
 -- --------------------------------------------------------
 
@@ -124,44 +129,26 @@ CREATE TABLE IF NOT EXISTS `tblcmsgroups` (
   `Group_Id` int(11) NOT NULL AUTO_INCREMENT,
   `Group_Page_Id` int(11) NOT NULL,
   `Group_Position` int(11) NOT NULL,
-  `Group_Css_Class` varchar(255) COLLATE utf8_bin NOT NULL,
-  `Group_Css_Id` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Group_Css_Class` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `Group_Css_Id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`Group_Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=54 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tblcmsgroups`
 --
 
 INSERT INTO `tblcmsgroups` (`Group_Id`, `Group_Page_Id`, `Group_Position`, `Group_Css_Class`, `Group_Css_Id`) VALUES
-(37, 1, 2, 'bg-primary text-secondary', ''),
-(31, 1, 1, 'bg-primary ', ''),
-(36, 1, 0, 'bg-primary text-secondary', ''),
-(38, 1, 3, '', '');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `tblcmsnavbaritems`
---
-
-DROP TABLE IF EXISTS `tblcmsnavbaritems`;
-CREATE TABLE IF NOT EXISTS `tblcmsnavbaritems` (
-  `NavbarItem_Id` int(11) NOT NULL AUTO_INCREMENT,
-  `NavbarItem_Title` varchar(255) COLLATE utf8_bin NOT NULL,
-  `NavbarItem_Url` longtext COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`NavbarItem_Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Gegevens worden geëxporteerd voor tabel `tblcmsnavbaritems`
---
-
-INSERT INTO `tblcmsnavbaritems` (`NavbarItem_Id`, `NavbarItem_Title`, `NavbarItem_Url`) VALUES
-(1, 'Over ons', '#aboutus'),
-(2, 'Projecten', '#projects'),
-(3, 'Onze partners', '#partners'),
-(4, 'Contact', '#contact');
+(42, 10, -1, '', ''),
+(43, 10, -1, '', ''),
+(44, 10, -1, NULL, NULL),
+(48, 11, 1, 'bg-primary', ''),
+(47, 11, 4, '', 'partners'),
+(49, 11, 2, 'bg-primary text-white', 'about'),
+(50, 11, 0, '', ''),
+(51, 11, 5, 'bg-primary text-white', ''),
+(52, 11, 3, '', 'Projecten'),
+(53, 11, 6, '', 'contact');
 
 -- --------------------------------------------------------
 
@@ -172,19 +159,19 @@ INSERT INTO `tblcmsnavbaritems` (`NavbarItem_Id`, `NavbarItem_Title`, `NavbarIte
 DROP TABLE IF EXISTS `tblcmspages`;
 CREATE TABLE IF NOT EXISTS `tblcmspages` (
   `Page_Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Page_Title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Page_Title` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `Page_Url` text COLLATE utf8_bin,
   `Page_Visible` tinyint(1) DEFAULT '0',
   `Page_Description` text COLLATE utf8_bin,
   PRIMARY KEY (`Page_Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tblcmspages`
 --
 
 INSERT INTO `tblcmspages` (`Page_Id`, `Page_Title`, `Page_Url`, `Page_Visible`, `Page_Description`) VALUES
-(1, 'index', '/', 1, 'awdwakduwakldhawjkldhwakljdhawdkjwhadkjwahdkjawhdkjwahdkawjdhawkjdhawkjldhawkjhkdhawdklwad');
+(11, 'test', '/', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -218,15 +205,15 @@ INSERT INTO `tblrights` (`Right_Id`, `Right_Name`) VALUES
 
 DROP TABLE IF EXISTS `tblrolerights`;
 CREATE TABLE IF NOT EXISTS `tblrolerights` (
-  `Role_Id` int(11) NOT NULL,
-  `Right_Id` int(11) NOT NULL
+  `RoleRight_Role_Id` int(11) NOT NULL,
+  `RoleRight_Right_Id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tblrolerights`
 --
 
-INSERT INTO `tblrolerights` (`Role_Id`, `Right_Id`) VALUES
+INSERT INTO `tblrolerights` (`RoleRight_Role_Id`, `RoleRight_Right_Id`) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
@@ -274,14 +261,7 @@ CREATE TABLE IF NOT EXISTS `tblsessions` (
 --
 
 INSERT INTO `tblsessions` (`Session_Id`, `Session_User_Id`, `Session_Last_Active`) VALUES
-('m128qn3ggp43cdoij76o8qjrqc', 1, 1551947410),
-('19qrhnpulhtf8onpd56qgobns5', 1, 1551784792),
-('crgg8ekibcn3t6unl5lm0htpm7', 1, 1551111987),
-('v554mvktb8cemera96pd6b3oqe', 1, 1551720019),
-('cncivejdjtmjrhd8fepjdjsab7', 1, 1551536355),
-('8n2ho56n6i1t5rmfhv23heumj1', 1, 1552334614),
-('qh8s7ub8ht360dhhqhu85c6ued', 1, 1552343126),
-('ee42gfu0ptr31uu9mf25cl0i1e', 1, 1552383896);
+('jbhm1565s6fm9l5q15mrbohfmt', 1, 1555964195);
 
 -- --------------------------------------------------------
 
@@ -310,7 +290,7 @@ INSERT INTO `tblsettings` (`Setting_Id`, `Setting_Name`, `Setting_Value`, `Setti
 (4, 'server', 'smtp.gmail.com', 1, 'text', 'smtp'),
 (5, 'password', '98sHdcMj5wG98I2VtHew', 1, 'password', 'smtp'),
 (6, 'port', '587', 1, 'number', 'smtp'),
-(7, 'secure', '', 0, 'text', 'smtp'),
+(7, 'Test', '', 0, 'text', 'smtp'),
 (8, 'template', '/mail.html', 1, 'text', 'plg.contact');
 
 -- --------------------------------------------------------
@@ -321,16 +301,17 @@ INSERT INTO `tblsettings` (`Setting_Id`, `Setting_Name`, `Setting_Value`, `Setti
 
 DROP TABLE IF EXISTS `tbluserroles`;
 CREATE TABLE IF NOT EXISTS `tbluserroles` (
-  `User_Id` int(11) NOT NULL,
-  `Role_Id` int(11) NOT NULL
+  `UserRole_User_Id` int(11) NOT NULL,
+  `UserRole_Role_Id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tbluserroles`
 --
 
-INSERT INTO `tbluserroles` (`User_Id`, `Role_Id`) VALUES
-(1, 1);
+INSERT INTO `tbluserroles` (`UserRole_User_Id`, `UserRole_Role_Id`) VALUES
+(1, 1),
+(1, 2);
 
 -- --------------------------------------------------------
 
