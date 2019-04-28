9ab50762a13979555b6a8dca822e461f88d0b7c4
NEAL-DESKTOP\Neal-Geilen <nealgeilen@gmail.com>
2019-04-28 13:31:14 +0200

    Modals update

--- assets/js/Modals.js
+++ assets/js/Modals.js
@@ -1,24 +1,31 @@
 var Modals = (function() {
     /**
      * Main function.
-     * Setting set and event listeners are added
+     * Setting set and event are added
      * @param options
      * @constructor
      */
     function Modal( options ) {
-        $('.modal-backdrop').remove();
-        $('.Modals-Modal').remove();
+        aSelectors = {
+            Modal: '.Modals-Modal',
+            Modal_Footer: '.modal-footer',
+            Modal_Title: '.modal-title',
+            Modal_Body: '.modal-body',
+            Modal_Dialog: '.modal-dialog',
+            Modal_Content: '.modal-content',
+        };
+
+        $(aSelectors.Modal).remove();
         var settings = $.extend({
             Title : null,
             Message: null,
-            //lg || sm
             Size : null,
-            Theme: 'light',
             Buttons:
                 [
                     Buttons.confirm,
                     Buttons.cancel
                 ],
+            isForm: false,
             onConfirm : function () {},
             onDeny: function(){},
             onInit: function () {},
@@ -28,7 +35,7 @@ var Modals = (function() {
         if (!settings.Title){
             throw new Error('Modal title not given');
         }
-        if (!settings.Message){
+        if (!settings.Message) {
             throw  new Error('Modal message not given');
         }
 
@@ -46,32 +53,21 @@ var Modals = (function() {
 
 
 
-        modal = Build_Modal();
+        modal = Build_Modal(settings.isForm);
+        modal.find(aSelectors.Modal_Dialog).addClass(modal_size);
         //Set Bootsrap modal settings
         modal.modal({
             backdrop: 'static',
-            keyboard: true
+            keyboard: false
         });
-        modal.find('.modal-dialog').addClass(modal_size);
         //Set titel en message field
-        modal.find('.modal-title').text(settings.Title);
-        modal.find('.modal-body').html(settings.Message);
+        modal.find(aSelectors.Modal_Title).text(settings.Title);
+        modal.find(aSelectors.Modal_Body).html(settings.Message);
 
-        switch (settings.Theme) {
-            case 'dark' :
-                modal.find('.modal-header').css('background-color', '#1a2035');
-                modal.find('.modal-footer').css('background-color', '#1a2035');
-                modal.find('.modal-content').css('background-color', '#1a2035');
-                modal.find('.modal-content').css('color', '#fff');
-                break;
-            case 'light':
-                modal.find('.modal-content').css('background-color', '#f8f9fa');
-                break;
-        }
 
         //Add buttons to modal footer
         $.each(settings.Buttons, function (iKey, oButton) {
-            modal.find('.modal-footer').append(oButton);
+            modal.find(aSelectors.Modal_Footer).append(oButton);
         });
         //Before init & modal show
         settings.onInit.call(this , modal, settings);
@@ -81,21 +77,19 @@ var Modals = (function() {
 
         //Binding events
         $(modal.find('.modal-confirm')).on('click', function () {
-            if (!$(this).hasClass('disabled')){
-                settings.onConfirm.call(this , modal);
-                $('.modal-confirm').unbind();
-                modal.modal('hide');
-                $('.modal-backdrop').remove();
-                $('.Modals-Modal').remove();
+            if (settings.isForm){
+                settings.onConfirm.call(this , modal, modal.find('form').serializeArray());
+            } else{
+                settings.onConfirm.call(this , modal, false);
             }
+            modal.modal('hide');
+            $(aSelectors.Modal).remove();
         });
 
         $(modal.find('.modal-denied')).on('click', function () {
             settings.onDeny.call(this, modal);
-            $('.modal-denied').unbind();
             modal.modal('hide');
-            $('.modal-backdrop').remove();
-            $('.Modals-Modal').remove();
+            $(aSelectors.Modal).remove();
         });
 
 
@@ -106,11 +100,22 @@ var Modals = (function() {
      * @return {jQuery|HTMLElement}
      * @constructor
      */
-    var Build_Modal = function (){
+    var Build_Modal = function (isForm = false){
         var oElement = $(Elements.Modal.dialog);
-        oElement.find('.modal-body').before(Elements.Modal.header);
-        oElement.find('.modal-body').after(Elements.Modal.footer);
-        oElement.find('.modal-title').after(Elements.Modal.close);
+        oElement.find(aSelectors.Modal_Body).before(Elements.Modal.header);
+        oElement.find(aSelectors.Modal_Body).after(Elements.Modal.footer);
+        if (isForm){
+            oConetent = oElement.find(aSelectors.Modal_Content).html();
+            oElement.find(aSelectors.Modal_Content).html(null);
+            oForm = $('<form>').append(oConetent);
+            oForm.validator();
+            oForm.submit(function (e) {
+                e.preventDefault();
+            });
+            oElement.find(aSelectors.Modal_Content).append(oForm);
+        }
+
+        oElement.find(aSelectors.Modal_Title).after(Elements.Modal.close);
         return oElement;
     };
 
@@ -119,10 +124,10 @@ var Modals = (function() {
      * @type {{confirm: string, allow: string, deny: string, cancel: string}}
      */
     var Buttons = {
-        confirm: '<button class="btn modal-confirm">Oke</button>',
-        allow : '<button class="btn modal-confirm">Ja</button>',
-        deny: '<button class="btn btn-secondary modal-denied">Nee</button>',
-        cancel: '<button class="btn btn-secondary modal-denied">Annuleren</button>',
+        confirm: '<button class="btn modal-confirm" type="submit">Ok</button>',
+        allow : '<button class="btn modal-confirm" type="submit">Yes</button>',
+        deny: '<button class="btn modal-denied" type="reset">No</button>',
+        cancel: '<button class="btn modal-denied" type="reset">Cancel</button>',
     };
     /**
      * Dom Elements needed for Modal build up.
@@ -145,49 +150,30 @@ var Modals = (function() {
                 "</div>",
             footer: "<div class='modal-footer'></div>",
 
-            close : "<button type='button' class='modal-denied close' aria-hidden='true'>&times;</button>",
-
+            close : "<button type='reset' class='modal-denied close' type='reset' aria-hidden='true'>&times;</button>",
             custom_body: "<div class='row'><div class='modal-icon col-4'></div><div class='modal-message col-8'></div></div>"
         },
-        Icons :{
-            info : '<i class="material-icons">info</i>',
-            error : '<i class="material-icons">error_outline</i>',
-            warning: '<i class="material-icons">warning</i>',
-            success: '<i class="material-icons">check</i>'
-        }
 
     };
 
-    /**
-     * Color types for diffrent templates
-     * @type {{info: string, error: string, warning: string, success: string}}
-     */
-    var Colors = {
-        info : '#17a2b8',
-        error: '#dc3545',
-        warning: '#ffc107',
-        success: '#28a745'
-    };
     /**
      * Template builder & constructor
-     * @param sIcon
-     * @param sColor
      * @param oSettings
+     * @param sTheme
      */
-    var template = function (sIcon, sColor, oSettings) {
+    var template = function (oSettings, sTheme = null) {
         var settings = $.extend({
             onInit: function (oModal, oSettings) {
-                $(oModal).find('.modal-header').css('background-color', sColor);
-                $(oModal).find('.modal-content').css('border', '2px solid' + sColor);
-                $(oModal).find('.modal-body').html(null);
-                $(oModal).find('.modal-body').append(Elements.Modal.custom_body);
-                $(oModal).find('.modal-icon').append(sIcon).css('color', sColor);
-                $(oModal).find('.modal-message').append(oSettings.Message);
+                $(oModal).find(aSelectors.Modal_Body).html(null);
+                $(oModal).addClass(sTheme);
+                $(oModal).find(aSelectors.Modal_Body).append(Elements.Modal.custom_body);
+                $(oModal).find('.modal-message').html(oSettings.Message);
             }
         }, oSettings );
 
         Modal( settings );
     };
+
     /**
      * Inform template call
      * @param options
@@ -197,10 +183,10 @@ var Modals = (function() {
         var settings = $.extend({
             Buttons:
                 [
-                    $(Buttons.allow).css('background-color', Colors.info).text('Oké'),
+                    $(Buttons.allow),
                 ],
         }, options);
-        template(Elements.Icons.info, Colors.info, settings);
+        template(settings, 'Modal-Info');
     };
 
     /**
@@ -212,10 +198,10 @@ var Modals = (function() {
         var settings = $.extend({
             Buttons:
                 [
-                    $(Buttons.confirm).css('background-color', Colors.error),
+                    $(Buttons.confirm),
                 ],
         }, options);
-        template(Elements.Icons.error, Colors.error, settings);
+        template(settings, 'Modal-Error');
     };
 
     /**
@@ -227,11 +213,11 @@ var Modals = (function() {
         var settings = $.extend({
             Buttons:
                 [
-                    $(Buttons.allow).css('background-color', Colors.warning),
-                    $(Buttons.deny),
+                    $(Buttons.allow),
+                    $(Buttons.deny)
                 ],
         }, options);
-        template(Elements.Icons.warning, Colors.warning, settings);
+        template(settings, 'Modal-Warning');
     };
 
     /**
@@ -243,18 +229,13 @@ var Modals = (function() {
         var settings = $.extend({
             Buttons:
                 [
-                    $(Buttons.confirm).css('background-color', Colors.success)
+                    $(Buttons.confirm)
                 ],
         }, options);
-        template(Elements.Icons.success, Colors.success, settings);
+        template(settings, 'Modal-Success');
     };
 
-    var edit = function ( options ) {
-        var setting = $.extend({
-            Size : 'large'
-        }, options);
-        Modal(setting);
-    };
+
 
     return {
         /*Custom call*/
@@ -264,7 +245,6 @@ var Modals = (function() {
         Error: Error,
         Warning: Warning,
         Success: Success,
-        Edit : edit,
 
         Buttons: Buttons
     };
