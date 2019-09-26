1b98ec13a19bb2b81b1a90514be61a9ad5ee637f
NEAL-DESKTOP\Neal-Geilen <nealgeilen@gmail.com>
2019-09-26 19:02:05 +0200

    menu load bug fix

--- _api/cms/Menu.php
+++ _api/cms/Menu.php
@@ -6,20 +6,19 @@ $request = new \NG\Client\RequestHandler();
 $Pages = new \Arura\CMS\Page\Page();
 $request->setRight(Rights::CMS_MENU);
 $request->TriggerEvent();
-$response->isDebug(true);
 $request->setRequestMethod('POST');
 $request->sandbox(function ($aData) use ($response){
     $aOutcome=[];
     switch ($aData['type']){
         case 'get':
-            $aOutcome = json_array_decode(file_get_contents(__SITE__ . '\Templates\menu.json'));
+            $aOutcome = json_array_decode(file_get_contents(__SITE__ . DIRECTORY_SEPARATOR . 'Templates'.DIRECTORY_SEPARATOR.'menu.json'));
             break;
         case 'set':
             $aNavData =$aData['NavData'];
             if (empty($aNavData)){
                 $aNavData = [];
             }
-            file_put_contents(__SITE__ . '\Templates\menu.json', json_encode($aNavData));
+            file_put_contents(__SITE__ . DIRECTORY_SEPARATOR . 'Templates'.DIRECTORY_SEPARATOR.'menu.json', json_encode($aNavData));
             break;
     }
     $response->exitSuccess($aOutcome);
