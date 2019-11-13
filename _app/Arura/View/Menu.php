2d5291eb83686a161e0120eec6defe8104384076
NEAL-DESKTOP\Neal-Geilen <nealgeilen@gmail.com>
2019-11-13 12:42:42 +0100

    bug class bug

--- _app/Arura/View/Menu.php
+++ _app/Arura/View/Menu.php
@@ -5,7 +5,7 @@ namespace Arura\View;
 
 class  Menu{
 
-    protected static $sFilePath = __ARURA_TEMPLATES__ . 'menu.json';
+    protected static $sFilePath = __WEB_TEMPLATES__ . 'menu.json';
 
     public static function getMenuStructure(){
         return json_array_decode(file_get_contents(self::$sFilePath));
