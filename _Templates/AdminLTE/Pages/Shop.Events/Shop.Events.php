e35a8fdfec625ea1546c1e24ab7416cd762f07b5
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2019-12-18 09:36:09 +0100

    tblEvents.json added

--- _Templates/AdminLTE/Pages/Shop.Events/Shop.Events.php
+++ _Templates/AdminLTE/Pages/Shop.Events/Shop.Events.php
@@ -1,7 +1,13 @@
 <?php
 use Arura\Dashboard\Page;
 $oSmarty =  Page::getSmarty();
+$db = new \NG\Database();
 if (isset($_GET["c"])){
+
+    if (isset($_POST["awdawdawd"])){
+        $e = \Arura\Shop\Events\Event::Create($_POST);
+    }
+    $oSmarty->assign("aUsers", $db->fetchAll("SELECT * FROM tblUsers"));
     return Page::getHtml(__DIR__ . "/Shop.Events.Create.html");
 } else if (isset($_GET["e"]) && !empty($_GET["e"])){
     return Page::getHtml(__DIR__ . "/Shop.Events.Edit.html");
