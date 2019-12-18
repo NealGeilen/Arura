fbaea86520f0fe96b1a0ad0920c21ba8ffb92a2d
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2019-12-18 10:58:37 +0100

    tblEventTickets.json added

--- _Templates/AdminLTE/Pages/Shop.Events/Shop.Events.php
+++ _Templates/AdminLTE/Pages/Shop.Events/Shop.Events.php
@@ -7,14 +7,20 @@ if (isset($_GET["c"])){
     if (isset($_POST["Event_Name"])){
         $_POST["Event_Start_Timestamp"] = strtotime($_POST["Event_Start_Timestamp"]);
         $_POST["Event_End_Timestamp"] = strtotime($_POST["Event_End_Timestamp"]);
+        $_POST["Event_IsActive"] = 0;
+        $_POST["Event_IsVisible"] = 0;
         $e = \Arura\Shop\Events\Event::Create($_POST);
     }
     $oSmarty->assign("aUsers", $db->fetchAll("SELECT * FROM tblUsers"));
     return Page::getHtml(__DIR__ . "/Shop.Events.Create.html");
 } else if (isset($_GET["e"]) && !empty($_GET["e"])){
+    $oSmarty->assign("aUsers", $db->fetchAll("SELECT * FROM tblUsers"));
+    $oSmarty->assign("aEvent", $db->fetchRow("SELECT * FROM tblEvents WHERE Event_Id = :Event_Id", ["Event_Id" => $_GET["e"]]));
+    $c = new \Arura\Crud();
+    $oSmarty->assign("sTicketsCrud", );
     return Page::getHtml(__DIR__ . "/Shop.Events.Edit.html");
 }
-
+$oSmarty->assign("aEvents", $db->fetchAll("SELECT * FROM tblEvents"));
 return Page::getHtml(__DIR__);
 
 
--- _app/Arura/Crud.php
+++ _app/Arura/Crud.php
@@ -1,29 +1,23 @@
 <?php
 namespace Arura;
-
 use NG\Database;
+use NG\User\User;
 
 class Crud extends Database {
 
-    private $aDataFile;
+    protected $sHtml = "";
+    protected $aDataFile;
+    protected $aColumnInfo;
+    protected $aParas;
 
-    private $aColumnInfo;
-
-    private $sHtml = "";
-
-    public function __construct(
-        $sDataFile
-    )
-    {
+    public function __construct($aData,$aParas){
+        $this->aDataFile = $aData;
+        $this->aParas = $aParas;
         parent::__construct();
-        $this->aDataFile = json_array_decode(file_get_contents($sDataFile));
-        foreach ($this->fetchAll('SHOW columns FROM '. $this->aDataFile["table"]) as $column){
-            $this -> aColumnInfo[$column["Field"]] = $column;
-        }
-        if (isset($_GET['action'])){
-            $this->Actions();
-        }
-        $this->buildTable();
+    }
+
+    protected function getUrl(){
+        return $_SERVER["REDIRECT_URL"] . http_build_query($this->aParas);
     }
 
     private function Actions(){
@@ -33,37 +27,34 @@ class Crud extends Database {
                     $this -> sHtml = $this->buildInputField([],'save') . $this->sHtml;
                     break;
                 case 'save':
-                    if (isset($_POST[$this->aDataFile["primarykey"]])){
-                        unset($_POST[$this->aDataFile["primarykey"]]);
-                    }
                     foreach ($_POST as $sName => $sValue){
                         if (str_contains("auto_increment",$this->aColumnInfo[$sName]['Extra'])){
                             unset($_POST[$sName]);
                         }
                     }
                     $this->createRecord($this->aDataFile["table"], $_POST);
+                    header('Location:' . $this->getUrl());
                     break;
                 case 'edit':
                     if (isset($_GET['_key'])){
-                        $this -> sHtml = $this->buildInputField($this->fetchRow('SELECT * FROM ' . $this->aDataFile["table"] . ' WHERE ' . $this->aDataFile["primarykey"] ." = :". $this->aDataFile["primarykey"], [$this->aDataFile["primarykey"] => $_GET['_key']]),'update') . $this->sHtml;
+                        $this -> sHtml = $this->buildInputField($this->fetchRow("SELECT * FROM ". $this->aDataFile["table"] . " WHERE " . $this->aDataFile["primarykey"] ." = :" . $this->aDataFile["primarykey"], [$this->aDataFile["primarykey"] => $_GET["KEY"]]), "update"). $this->sHtml;
                     }
                     break;
                 case 'update':
-                    if (isset($_POST[$this->aDataFile["primarykey"]])){
-                        $this->updateRecord($this->aDataFile["table"], $_POST, $this->aDataFile["primarykey"]);
-                        if ($this->isQuerySuccessful()){
-                            header('Location:' . $_SERVER["REDIRECT_URL"]);
-                        }
+                    $this->updateRecord($this->aDataFile["table"], $_POST, $this->aDataFile["primarykey"]);
+                    if ($this->isQuerySuccessful()){
+                        header('Location:' . $this->getUrl());
                     }
                     break;
                 case 'delete':
-                    if (isset($_GET['_key'])){
-                        $this->query('DELETE FROM '. $this->aDataFile["table"] . ' WHERE ' . $this->aDataFile["primarykey"] . ' = :'.$this->aDataFile["primarykey"], [$this->aDataFile["primarykey"] => $_GET['_key']]);
+                    $this->query('DELETE FROM '. $this->aDataFile["table"] . ' WHERE ' . $this->aDataFile["primarykey"] . ' = :'.$this->aDataFile["primarykey"], [$this->aDataFile["primarykey"] => $_GET['_key']]);
+                    if ($this->isQuerySuccessful()){
+                        header('Location:' . $this->getUrl());
                     }
                     break;
             }
         } catch (\Exception $e){
-            \header('Location:' . $_SERVER["REDIRECT_URL"]);
+            header('Location:' . $this->getUrl());
         }
     }
 
@@ -72,70 +63,93 @@ class Crud extends Database {
      */
     public function getHTMLTable()
     {
+        if (isset($_GET["action"])){
+            $this->Actions();
+        }
+        $this->buildTable();
         return $this->sHtml;
     }
 
     private function buildTable(){
-        $sQuery = "SELECT ";
-        foreach ($this->aDataFile["columns"] as $tag => $aData){
-            $sQuery .= $tag . ", ";
-        }
-        $sQuery = trim($sQuery,", ");
-
-        $sQuery .= " FROM " . $this->aDataFile["table"];
-        $aData = $this->fetchAll($sQuery);
-        $this->sHtml .= "<a href='".$_SERVER["REDIRECT_URL"]."?action=create'>Toevoegen</a>";
+        $aData = $this->fetchAll("SELECT * FROM ". $this->aDataFile["table"]);
         $this->sHtml .= "<table class='table'>";
-        $this->sHtml .= "<tr>";
+        $this->sHtml .= "<thead><tr>";
         foreach ($this->aDataFile["columns"] as $Column){
             $this->sHtml .= "<th>" . $Column['name'] . "</th>";
         }
-        $this->sHtml .= "</tr>";
+        $this->sHtml .= "<th>";
+        if ($_GET["action"] !== "create" && $_GET["action"] !== "edit"){
+            $this->sHtml .= "<a class='btn btn-primary' href='".$this->getUrl()."&action=create'><i class=\"fas fa-plus\"></i></a>";
+        }
+        $this->sHtml .= "</th></tr></thead><tbody>";
         foreach ($aData as $record){
             $this->sHtml  .= "<tr>";
-            foreach ($record as $column){
-                $this->sHtml  .= "<td>" . $column . "</td>";;
+            foreach ($record as $tag => $column){
+                switch ($this->aDataFile["columns"][$tag]["type"]){
+                    case "dropdown":
+                        $this->sHtml  .= "<td>" . $this->aDataFile["columns"][$tag]["options"][$column] . "</td>";
+                        break;
+                    default:
+                        $this->sHtml  .= "<td>" . $column . "</td>";
+                        break;
+
+                }
             }
             $this->sHtml .= "<td>".$this->getActionButtons($record[$this->aDataFile["primarykey"]])."</td>";
             $this->sHtml  .= "</tr>";
 
         }
-        $this->sHtml  .= "</table>";
+        if (count($aData) === 0){
+            $this->sHtml .= "<td colspan='".count($this->aDataFile["columns"])."'><div class='alert alert-info'>Er zijn geen gegegevens aanwezig</div></td>";
+        }
+        $this->sHtml  .= "</tbody></table>";
     }
 
     protected function getActionButtons($iRowId = null){
-        $s = "";
-        $aButtons = [
-            "Verwijder"     => "delete",
-            "Veranderen"    => "edit"
-        ];
-        foreach ($aButtons as $name =>  $aButton){
-            $s .= "<a href='".$_SERVER["REDIRECT_URL"]."?action=".$aButton."&_key=".$iRowId."' class='btn btn-primary'>".$name."</a>";
-        }
+        $s = "<div class='btn-group'>";
+        $s .= "<a href='".$this->getUrl()."&action=delete&_key=".$iRowId."' class='btn btn-danger'><i class=\"fas fa-trash\"></i></a>";
+        $s .= "<a href='".$this->getUrl()."&action=edit&_key=".$iRowId."' class='btn btn-primary'><i class=\"fas fa-pen\"></i></a>";
+        $s .= "</div>";
         return $s;
     }
 
     protected  function buildInputField($aData = [],$sAction = null){
-        $sHtml = "<form method='post' action='".$_SERVER["REDIRECT_URL"]."?action=".$sAction."'><table>";
+        $sHtml = "<h2>".(($sAction==="save") ? "Toevoegen" : "Bewerken")."</h2>";
+        $sHtml .= "<form method='post' action='".$this->getUrl()."&action=".$sAction."' class='form-row bg-secondary p-1 border-4 border-dark rounded'>";
         foreach ($this->aDataFile["columns"] as $tag => $column){
-            $value = "";
-            $sInputGroup = "<tr>";
-            if (!empty($aData)){
+            $value = null;
+            if (isset($aData[$tag])){
                 $value = $aData[$tag];
             }
+            $sInputGroup = "<div class='form-group col-xl-3 col-md-3 col-sm-6 col-12'>";
             if (!str_contains("auto_increment",$this->aColumnInfo[$tag]["Extra"])){
-                $sInputGroup .= "<td><label>".$column['name']."</label></td>";
-                $sInputGroup.= "<td><input type='".$column["type"]."' name='".$tag."' value='".$value."'/></td>";
+                $sInputGroup .= "<label>".$column['name']."</label>";
+                switch ($column["type"]){
+                    case "dropdown":
+                        $sInputGroup .= "<select class='form-control' value='".$value."' name='".$tag."'>";
+                        if (isset($column["options"])){
+                            foreach ($column["options"] as $i => $x){
+                                $sInputGroup .= "<option value='".$i."'>" . $x . "</option>";
+                            }
+                        }
+                        $sInputGroup .= "</select>";
+                        break;
+                    default:
+                        $sInputGroup.= "<input type='".$column["type"]."' name='".$tag."' value='".$value."' class='form-control' ".(($sAction === "update" && $tag === $this->aDataFile["primarykey"]) ? "readonly" : null)."/>";
+                        break;
+                }
+                ;
+
             } else {
-                $sInputGroup = "<td><input type='hidden' name='".$tag."' value='".$value."'/></td>";
+                $sInputGroup = "<input type='hidden' name='".$tag."' value='".$value."'/>";
             }
-            $sInputGroup .= "</tr>";
+            $sInputGroup .= "</div>";
             $sHtml .= $sInputGroup;
         }
 
-        $sHtml .= "<tr><td><input type='submit'></td><td><input type='reset'></td></tr>";
+        $sHtml .= "<div class='col-md-12'><div class='btn-group'><input type='submit' class='btn btn-success' value='Opslaan'><a class='btn btn-danger' href='".$_SERVER["REDIRECT_URL"]."?t=".$this->oAdmin->getId()."'>Annuleren</a></div></div>";
         $sHtml .= "";
-        $sHtml .= "</table></form>";
+        $sHtml .= "</form>";
         return $sHtml;
     }
 
@@ -143,11 +157,4 @@ class Crud extends Database {
     {
         return $this->getHTMLTable();
     }
-
-//    public static function Build($sTable,$sDatabase = null,$sHost = null, $sUser = null,$sPassword = null){
-//        $Dump = new self($sTable,$sDatabase,$sHost,$sUser,$sPassword);
-//        $Dump -> buildTable();
-//        return $Dump;
-//    }
-
 }
