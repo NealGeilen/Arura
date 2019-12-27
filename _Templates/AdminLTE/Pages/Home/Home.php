61b161b69e75dbf5b36fb2b611a2e358a2556c03
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2019-12-27 12:21:51 +0100

    Home page update

--- _Templates/AdminLTE/Pages/Home/Home.php
+++ _Templates/AdminLTE/Pages/Home/Home.php
@@ -8,7 +8,7 @@ if (\Arura\Permissions\Restrict::Validation(Rights::ARURA_USERS)){
 $oSmarty->assign("iPageCount", (\Arura\Permissions\Restrict::Validation(Rights::CMS_PAGES) ? $db->fetchRow("SELECT COUNT(Page_Id) AS ROW_COUNT FROM tblCmsPages WHERE Page_Visible = 1")["ROW_COUNT"] : null));
 $oSmarty->assign("iUserCount", (\Arura\Permissions\Restrict::Validation(Rights::ARURA_USERS) ? $db->fetchRow("SELECT COUNT(Session_Id) AS ROW_COUNT FROM tblSessions")["ROW_COUNT"] : null));
 $oSmarty->assign("aSecureTables", (\Arura\Permissions\Restrict::Validation(Rights::SECURE_ADMINISTRATION) ? \Arura\SecureAdmin\SecureAdmin::getAllTablesForUser(\Arura\User\User::activeUser()) : null));
-
-
+$oSmarty->assign("aEvents", (\Arura\Permissions\Restrict::Validation(Rights::SHOP_EVENTS_MANAGEMENT) ? Arura\Shop\Events\Event::getAllEvents() : null));
+$oSmarty->assign("aPayments", (\Arura\Permissions\Restrict::Validation(Rights::SHOP_PAYMENTS) ? \Arura\Shop\Payment::getPaymentsFromLast(24) : null));
 
 return Page::getHtml(__DIR__);
