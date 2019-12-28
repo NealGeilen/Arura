1b478b425ce97a75db784ea452e6c0f6c8898eed
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2019-12-28 12:05:38 +0100

    Remodal settings page

--- _Templates/AdminLTE/Pages/Arura.Settings/Arura.Settings.php
+++ _Templates/AdminLTE/Pages/Arura.Settings/Arura.Settings.php
@@ -4,7 +4,7 @@ use Arura\Dashboard\Page;
 use Arura\Database;
 
 $db = new Database();
-$aSettings = $db -> fetchAll('SELECT * FROM tblSettings ORDER BY Setting_plg');
+$aSettings = $db -> fetchAll('SELECT * FROM tblSettings ORDER BY Setting_Plg, Setting_Name');
 $aList = [];
 foreach ($aSettings as $i =>$setting){
     $aList[$setting["Setting_Plg"]][] = $setting;
