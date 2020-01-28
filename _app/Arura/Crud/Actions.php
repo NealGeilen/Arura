0b382472c2bc7fc6c6eaf84e8f1d86d62e848799
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-01-28 17:02:34 +0100

    New Crud system added

---
+++ _app/Arura/Crud/Actions.php
@@ -0,0 +1,10 @@
+<?php
+namespace Arura\Crud;
+
+class Actions{
+    const CREATE = "create";
+    const SAVE = "save";
+    const EDIT = "edit";
+    const UPDATE = "update";
+    const DELETE = "delete";
+}
