40bfaffe0fd8109b3fdb84cdd8289e86ab4f1f9d
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-02-12 16:57:43 +0100

    Sidebar added

--- _Templates/AdminLTE/Pages/Cms.Page.Content/Cms.Page.Content.php
+++ _Templates/AdminLTE/Pages/Cms.Page.Content/Cms.Page.Content.php
@@ -12,4 +12,4 @@ $aPage = $db -> fetchRow('SELECT * FROM tblCmsPages WHERE Page_Id = ? ',
         (int)$_GET['c']
     ]);
 $smarty -> assign('aCmsPage', $aPage);
-Page::setSideBar(__DIR__ . "Cms.Page.Content.Sidebar.tpl");
+Page::setSideBar(__DIR__ . DIRECTORY_SEPARATOR ."Cms.Page.Content.Sidebar.tpl");
