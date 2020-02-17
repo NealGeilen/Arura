46133e45a29b3a41d6dae3cf3c91ea5715498978
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-02-17 10:18:14 +0100

    Clean template change

--- api/filemanger/upload.php
+++ api/filemanger/upload.php
@@ -1,12 +1,18 @@
 <?php
+
+use Arura\Client\RequestHandler;
+use Arura\Client\ResponseHandler;
+use Arura\FileManger\FileManger;
+
 require_once __DIR__ . '/../../_app/autoload.php';
-$response = new \Arura\Client\ResponseHandler();
-$request = new \Arura\Client\RequestHandler();
+$response = new ResponseHandler();
+$request = new RequestHandler();
+$response->isDebug(true);
 $request->setRight(Rights::FILES_UPLOAD);
 $request->TriggerEvent();
 $request->setRequestMethod('POST');
 $request->sandbox(function ($aData) use ($response){
-    $Manger = new \Arura\FileManger\FileManger();
+    $Manger = new FileManger();
     $Manger->uploadFiles($aData['dir']);
     $response->exitSuccess($aData);
 });
