c82f44b6e636369309f3deb43f5bb5ef1f8615f0
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-02-25 10:20:52 +0100

    Secureadmin update

--- api/secureadmin/create.php
+++ api/secureadmin/create.php
@@ -1,14 +1,20 @@
 <?php
+
+use Arura\Client\RequestHandler;
+use Arura\Client\ResponseHandler;
+use Arura\SecureAdmin\SecureAdmin;
+use Arura\User\User;
+
 require_once __DIR__ . '/../../_app/autoload.php';
-$response = new \Arura\Client\ResponseHandler();
-$request = new \Arura\Client\RequestHandler();
-$request->TriggerEvent();
+$response = new ResponseHandler();
+$request = new RequestHandler();
 $request->setRequestMethod('POST');
 $request->setRight(Rights::SECURE_ADMINISTRATION_CREATE);
 $request->sandbox(function ($aData) use ($response, $request) {
+    $request->TriggerEvent();
     foreach ($_FILES as $file){
         if ($file["type"] === "application/json"){
-            $oSecure = \Arura\SecureAdmin\SecureAdmin::Create(json_array_decode(file_get_contents($file["tmp_name"])), \Arura\User\User::activeUser());
+            $oSecure = SecureAdmin::Create(json_array_decode(file_get_contents($file["tmp_name"])), User::activeUser());
         }
     }
 });
