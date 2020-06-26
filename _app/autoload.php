992279cbb6fbedc9be46ee2480529907cedf0b2f
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-06-26 16:40:44 +0200

    ignore update

--- _app/autoload.php
+++ _app/autoload.php
@@ -6,11 +6,28 @@ use Arura\Settings\Application;
 define("__ARURA__DIR_NAME__", "dashboard");
 define('__ROOT__',              $_SERVER['DOCUMENT_ROOT']);
 define('__WEB__ROOT__',         $_SERVER['DOCUMENT_ROOT']);
-require_once __WEB__ROOT__ . "/_config.php";
-define('__SETTINGS__',          __APP_ROOT__    . DIRECTORY_SEPARATOR. 'settings'   . DIRECTORY_SEPARATOR);
-define('__VENDOR__',            __WEB__ROOT__   . DIRECTORY_SEPARATOR. 'vendor'     . DIRECTORY_SEPARATOR);
+require_once __ROOT__ . "/_config.php";
 
-define('__ARURA__ROOT__',       __VENDOR__ ."arura". DIRECTORY_SEPARATOR .  __ARURA__DIR_NAME__ . DIRECTORY_SEPARATOR);
+if (!defined("DEV_MODE")){
+    define("DEV_MODE", false);
+}
+
+define('__SETTINGS__',          __APP_ROOT__    . DIRECTORY_SEPARATOR. 'settings'   . DIRECTORY_SEPARATOR);
+
+if (DEV_MODE){
+    define('__ARURA__ROOT__',       __WEB__ROOT__ .  DIRECTORY_SEPARATOR .  __ARURA__DIR_NAME__ . DIRECTORY_SEPARATOR);
+    define('__VENDOR__',            __ARURA__ROOT__. 'vendor'     . DIRECTORY_SEPARATOR);
+    if(is_file(__VENDOR__ . "autoload.php")){
+        require_once __VENDOR__ . "autoload.php";
+    } else  {
+        throw new Error("composer not installed", 500);
+    }
+} else {
+    define('__VENDOR__',            __WEB__ROOT__   . DIRECTORY_SEPARATOR. 'vendor'     . DIRECTORY_SEPARATOR);
+    define('__ARURA__ROOT__',       __VENDOR__ ."arura". DIRECTORY_SEPARATOR .  __ARURA__DIR_NAME__ . DIRECTORY_SEPARATOR);
+}
+
+
 define("__ARURA_TEMPLATES__",   __ARURA__ROOT__ .DIRECTORY_SEPARATOR. '_Templates' . DIRECTORY_SEPARATOR);
 define('__APP__',               __ARURA__ROOT__ . DIRECTORY_SEPARATOR. '_app'       . DIRECTORY_SEPARATOR);
 define('__DATAFILES__',         __ARURA__ROOT__   . DIRECTORY_SEPARATOR . '_app/Resources/DataFiles'  . DIRECTORY_SEPARATOR);
@@ -20,6 +37,7 @@ define('__FILES__',             __WEB__ROOT__   . DIRECTORY_SEPARATOR. 'files'  
 define("__WEB_TEMPLATES__",     __WEB__ROOT__   . DIRECTORY_SEPARATOR . "Templates" . DIRECTORY_SEPARATOR);
 define('__RESOURCES__',         __WEB__ROOT__   . DIRECTORY_SEPARATOR . "_app" . DIRECTORY_SEPARATOR     . 'Resources'  . DIRECTORY_SEPARATOR);
 
+
 $GLOBALS["Querys"] = [];
 session_set_cookie_params(0, "/", "");
 Sessions::Start();
@@ -28,3 +46,4 @@ if ((int)Application::get("arura", "Debug")){
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
+exit;
