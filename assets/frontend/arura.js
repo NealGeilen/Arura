f899530ef6bfd2651b10897b7e48e24b7ff0e958
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-07-06 17:44:24 +0200

    Alerts update

--- assets/frontend/arura.js
+++ assets/frontend/arura.js
@@ -179,7 +179,13 @@ Arura = {
                     from: "bottom",
                     align: "right"
                 },
-                type:'success'
+                type:'success',
+                template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0} bg-{0} rounded border-0 text-white" role="alert">' +
+                    '<span data-notify="icon"></span> ' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span data-notify="message">{2}</span>' +
+                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                    '</div>'
             };
             $.notify({message:sMessage},oSettings);
         },
@@ -189,7 +195,13 @@ Arura = {
                     from: "bottom",
                     align: "right"
                 },
-                type:'danger'
+                type:'danger',
+                template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0} bg-{0} rounded border-0 text-white" role="alert">' +
+                    '<span data-notify="icon"></span> ' +
+                    '<span data-notify="title">{1}</span> ' +
+                    '<span data-notify="message">{2}</span>' +
+                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                    '</div>'
             };
             $.notify({message:sMessage},oSettings);
         }
