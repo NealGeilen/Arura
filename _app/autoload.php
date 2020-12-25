51a1f44b48afaaa470cf32d6f8b64acf5e6bb2e2
Neal Geilen <nealgeilen@gmail.com>
2020-12-25 17:49:22 +0100

    Trying some cache shit

--- _app/autoload.php
+++ _app/autoload.php
@@ -58,6 +58,7 @@ define('__RESOURCES__',         __WEB__ROOT__   . DIRECTORY_SEPARATOR . "_app" .
 define("__STANDARD_MODULES__" , __ARURA_TEMPLATES__ . "Modules" . DIRECTORY_SEPARATOR);
 define("__CUSTOM_MODULES__", __WEB_TEMPLATES__ . "Custom" . DIRECTORY_SEPARATOR. "Modules". DIRECTORY_SEPARATOR);
 Page::$smarty = new Smarty();
+session_cache_limiter("public");
 Sessions::Start();
 if ((int)Application::get("arura", "Debug")){
     ini_set('display_errors', 1);
