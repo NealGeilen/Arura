18f342f068ad4c0fe91b35b9b06a084ce760f25b
Neal Geilen <nealgeilen@gmail.com>
2021-01-18 20:45:46 +0100

    Bugfixes

--- _app/Arura/Pages/CMS/ContentBlock.php
+++ _app/Arura/Pages/CMS/ContentBlock.php
@@ -76,7 +76,7 @@ class ContentBlock extends Modal{
      */
     public function set($aBlock, $excludValue = true){
         $aBlock['Content_Id'] = $this->getId();
-        if ((int)$aBlock["Content_Addon_Id"]>0){
+        if ((int)$aBlock["Content_Addon_Id"]>0 && !$excludValue){
             if (!isset($aBlock["Content_Value"])){
                 $aBlock["Content_Value"] = "";
             }
