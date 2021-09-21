df6d360404b563c5c3d3f81cc9aef433f58b6316
Neal Geilen <44571222+NealGeilen@users.noreply.github.com>
2021-09-21 12:19:58 +0200

    Testing

--- App/Router.php
+++ App/Router.php
@@ -28,6 +28,7 @@ try {
     $oRouter->set404(function (){
         Page::pageNotFound();
     });
+    dd($oRouter->getCurrentUri());
     $aUrl = parse_url($_SERVER["REQUEST_URI"]);
     if (isset($aUrl["path"])){
         $aPath = explode("/", $aUrl["path"]);
